75dc8ca9325a36f69f552cdc210a2c36a4619fa7
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-08-23 10:18:46 -0700

    created newDeck fn that uses for loops to create semi full deck

--- cards/deck.go
+++ cards/deck.go
@@ -7,6 +7,23 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// any variable of type deck, gets access to the "print" method
+// this creates methods for types
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
