0879a7f814737767e67909414ce15b08c4b250d2
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-08-26 11:13:33 -0700

    turns deck into string w reciever func

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
+	fmt.Println(cards.toString())
 
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
 }
